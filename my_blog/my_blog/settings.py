57da76bd10b23f70d7b25ac75bcec89a171936ec
p_pltzhou <p_pltzhou@tencent.com>
2021-04-16 17:24:55 +0800

    allow_hosts

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@w#kt3rd95_zvqgw)r#ck3qq5rp$k=9urbu8$sabl5x670iu@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '1.14.43.98']
 
 
 # Application definition
